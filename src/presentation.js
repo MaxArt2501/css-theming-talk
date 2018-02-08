c4347f2252161ec5fd8cda2fd5e3cbcc02f7da65
Massimo Artizzu <massimo.artizzu@antreem.it>
2018-02-08 19:30:58 +0100

    Separated theme from presentation file

--- src/presentation.js
+++ src/presentation.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import { Deck } from 'spectacle';
 
-// Import theme
-import createTheme from 'spectacle/lib/themes/default';
-
 import './style.css';
 import 'normalize.css';
 
 import { Cover } from './slides/cover';
 import { WhoIAm } from './slides/whoiam';
 
-const theme = createTheme(
-  {
-    primary: '#f60',
-    background: '#fff',
-    text: '#595959',
-    alt: '#03a9fc'
-  },
-  {
-    primary: 'Raleway',
-    secondary: 'system-ui'
-  }
-);
+import { theme } from './theme';
+
 
 export default class Presentation extends React.Component {
   render() {
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import createTheme from 'spectacle/lib/themes/default';
+
+export const theme = createTheme(
+  {
+    primary: '#f60',
+    background: '#fff',
+    text: '#595959',
+    alt: '#03a9fc'
+  },
+  {
+    primary: 'Raleway',
+    secondary: 'system-ui'
+  }
+);
