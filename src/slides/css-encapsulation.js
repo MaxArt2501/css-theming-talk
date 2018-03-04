51d0bb97e03e9274c967c97d71ba23255a9ff1a1
Massimo Artizzu <maxart.x@gmail.com>
2018-03-05 00:32:29 +0100

    Finally some more slides

--- src/slides/css-encapsulation.js
+++ src/slides/css-encapsulation.js
@@ -13,19 +13,61 @@ import {
 } from 'spectacle';
 
 export const CSSEncapsulation = [
-  <Slide bgColor="background" bgImage="img/blue-red-pill.gif" align="center flex-start" id="style-encapsulation" key="style-encapsulation">
+  <Slide bgImage="img/blue-red-pill.gif" align="center flex-start" id="style-encapsulation" key="style-encapsulation">
     <Heading className="our-header" size={3}>
       <Text textColor="background" textSize="inherit">Style encapsulation</Text>
     </Heading>
   </Slide>,
-  <Slide bgColor="background" id="web-components" key="web-components">
+  <Slide id="web-components" key="web-components">
     <Heading className="our-header" size={3}>Web Components</Heading>
-    <List>
-      <ListItem>Achieve <em>true</em> encapsulation</ListItem>
+    <List textColor="text" textSize="30px" margin="3em 0 0 2em">
+      <ListItem>Achieve <em>true</em> encapsulation;</ListItem>
+      <ListItem>small DOM &rArr; simple CSS;</ListItem>
+      <ListItem>gives easy component reusability.</ListItem>
     </List>
   </Slide>,
-  <Slide bgColor="background" id="resusability" key="resusability">
+  <Slide id="resusability" key="resusability">
     <Heading className="our-header" size={3}>&hellip; component reusability?</Heading>
     <Image src="img/mashup-ui.png" height="400px"></Image>
+  </Slide>,
+  <Slide id="custom-example" key="custom-example">
+    <Heading className="our-header" size={3}>With parameters</Heading>
+    <CodePane lang="javascript" textSize="30px" margin="2em 0 1em" source={`
+customElements.define('submit-button', class extends HTMLElement {
+  constructor() {
+    super();
+    const root = this.attachShadow({ mode: 'open' });
+    const btn = document.createElement('button');
+    btn.textContent = this.textContent;
+    root.appendChild(btn);
+    btn.style.color = this.getAttribute('color');
+  }
+});
+`.trim()}></CodePane>
+    <CodePane lang="html" textSize="30px" source={`
+<submit-button color="navy">Send!</submit-button>
+`.trim()}></CodePane>
+  </Slide>,
+  <Slide id="custom-theme" key="custom-theme">
+    <Heading className="our-header" size={3}><code>theme</code> parameter</Heading>
+    <CodePane lang="javascript" textSize="25px" margin="2em 0 1em" source={`
+customElements.define('submit-button', class extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    ...
+    this.setTheme(this.getAttribute('theme'))
+  }
+  async setTheme(theme) {
+    const { buttonCss } = await import(\`./themes/\${theme}.js\`);
+    const style = document.createElement('style');
+    style.textContent = buttonCss;
+    this.shadowRoot.appendChild(style);
+  }
+});
+`.trim()}></CodePane>
+    <CodePane lang="html" textSize="25px" source={`
+<submit-button theme="material">Send!</submit-button>
+`.trim()}></CodePane>
   </Slide>
 ];
--- src/slides/first-approaches.js
+++ src/slides/first-approaches.js
@@ -11,7 +11,7 @@ import {
 } from 'spectacle';
 
 export const FirstApproaches = [
-  <Slide bgColor="background" id="bem-is" key="bem-is">
+  <Slide id="bem-is" key="bem-is" transition={[ 'fade' ]}>
     <Heading className="our-header" size={3}>BEM is&hellip;</Heading>
     <List textColor="text" margin="50px 0 0 100px">
       <ListItem>&hellip; effective;</ListItem>
@@ -19,14 +19,17 @@ export const FirstApproaches = [
       <ListItem>&hellip; <em>theme</em>-able;</ListItem>
       <ListItem>&hellip; nice to use (with the right tools).</ListItem>
     </List>
-    <Text margin="50px 0" textAlign="left" textColor="text">&hellip; but also:</Text>
-    <List textColor="text" margin="0 0 0 100px">
+  </Slide>,
+  <Slide id="bem-also-is" key="bem-also-is" transition={[ 'fade' ]}>
+    <Heading className="our-header" size={3}>&hellip; but also&hellip;</Heading>
+    <List textColor="text" margin="50px 0 0 100px">
       <ListItem>&hellip; verbose;</ListItem>
       <ListItem>&hellip; &ldquo;manual&rdquo;;</ListItem>
-      <ListItem>&hellip; not very readable.</ListItem>
+      <ListItem>&hellip; not very readable;</ListItem>
+      <ListItem>&hellip; BOOORING!</ListItem>
     </List>
   </Slide>,
-  <Slide bgColor="background" id="first-approaches" key="first-approaches">
+  <Slide id="first-approaches" key="first-approaches">
     <Heading className="our-header" size={3}>The <code>scoped</code> attribute (2012)</Heading>
     <CodePane lang="html" textSize="30px" source={`<div>
   <style scoped>
@@ -41,7 +44,7 @@ export const FirstApproaches = [
   ...
 </div>`}></CodePane>
   </Slide>,
-  <Slide bgColor="background" id="scoped-limitations" key="scoped-limitations">
+  <Slide id="scoped-limitations" key="scoped-limitations">
     <Heading className="our-header" size={3}>Limitations of <code>scoped</code></Heading>
     <List textColor="text" textSize="30px" margin="100px 0">
       <ListItem>It allows to style sub-components</ListItem>
@@ -52,19 +55,19 @@ export const FirstApproaches = [
       Support from Chrome was pulled in v36.
     </Text>
   </Slide>,
-  <Slide bgColor="background" bgImage="img/vue.svg" bgPosition="100% 100%" bgSize="auto 30%" bgRepeat="no-repeat" id="not-vues-scoped" key="not-vues-scoped">
+  <Slide bgImage="img/vue.svg" bgPosition="100% 100%" bgSize="auto 30%" bgRepeat="no-repeat" id="not-vues-scoped" key="not-vues-scoped">
     <Heading className="our-header" size={4}>
       It&rsquo;s <em>not</em> the same <code>scoped</code> of <Link
         href="https://vue-loader.vuejs.org/en/features/scoped-css.html">.vue files</Link>
     </Heading>
-    <CodePane lang="html" textSize="2.50px" margin="50px 0 0" width="500px" source={`<style scoped>
+    <CodePane lang="html" textSize="25px" margin="50px 0 0" width="500px" source={`<style scoped>
 h2 { font-size: 150%; }
 </style>
 <template>
   <div> <h2>Hello, world!</h2> </div>
 </template>`}></CodePane>
     <Text margin="10px 0">&#8681;</Text>
-    <CodePane lang="html" textSize="2.50px" width="500px" source={`<!-- Inside the <head> -->
+    <CodePane lang="html" textSize="25px" width="500px" source={`<!-- Inside the <head> -->
 <style type="text/css">
   h2[data-v-4c74d97c] { font-size: 150%; }
 </style>
