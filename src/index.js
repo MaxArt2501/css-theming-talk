d0c264367a573100774c20ae38220992534bbad6
Massimo Artizzu <maxart.x@gmail.com>
2018-03-13 00:38:18 +0100

    Added presenter support via ⬆️, ⬇️

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Presentation />, document.getElementById('root'));
 registerServiceWorker();
+
+const keyCodeRemap = {
+  40: 39,
+  38: 37
+};
+window.addEventListener('keydown', ({ keyCode }) => {
+  if (keyCode in keyCodeRemap) {
+      const e = new KeyboardEvent('keydown', { keyCode: keyCodeRemap[keyCode] });
+      window.dispatchEvent(e);
+  }
+});
