0b131058c139691fb99b2e3eaf1aff1d43317111
Massimo Artizzu <maxart.x@gmail.com>
2018-03-15 00:42:00 +0100

    Adjusted deck size not in presenter mode

--- src/index.js
+++ src/index.js
@@ -19,16 +19,20 @@ window.addEventListener('keydown', ({ keyCode }) => {
   }
 });
 
-const root = document.getElementById('root');
-const deckRoot = root.querySelector('.spectacle-deck');
-Object.assign(deckRoot.style, {
-  width: `${deckWidth}px`,
-  height: `${deckHeight}px`
-});
+const [ , protoQS ] = window.location.hash.split('?');
+const qsParts = protoQS ? protoQS.split('&').filter(Boolean) : [];
+if (!qsParts.includes('presenter')) {
+  const root = document.getElementById('root');
+  const deckRoot = root.querySelector('.spectacle-deck');
+  Object.assign(deckRoot.style, {
+    width: `${deckWidth}px`,
+    height: `${deckHeight}px`
+  });
 
-function setDeckRatio() {
-  const ratio = Math.min(window.innerWidth / deckWidth, window.innerHeight / deckHeight);
-  root.style.transform = `translate(calc(50vw - ${deckWidth * ratio / 2}px), calc(50vh - ${deckHeight * ratio / 2}px)) scale(${ratio})`;
+  function setDeckRatio() {
+    const ratio = Math.min(window.innerWidth / deckWidth, window.innerHeight / deckHeight);
+    root.style.transform = `translate(calc(50vw - ${deckWidth * ratio / 2}px), calc(50vh - ${deckHeight * ratio / 2}px)) scale(${ratio})`;
+  }
+  window.addEventListener('resize', setDeckRatio, { passive: true });
+  setDeckRatio();
 }
-window.addEventListener('resize', setDeckRatio, { passive: true });
-setDeckRatio();
