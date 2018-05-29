0e4933ea013849b1b3a6eabbbda70105c5fd5578
Massimo Artizzu <maxart.x@gmail.com>
2018-05-29 23:46:27 +0200

    Changed title for 🌡FEVR

--- src/slides/cover.js
+++ src/slides/cover.js
@@ -7,10 +7,10 @@ import { isCSSDay, isMFM, isFEVR } from '../status';
 export const Cover = (
   <Slide transition={['zoom']} bgImage="img/logo-antreem-contour.svg" bgSize="133% 133%" bgColor="brand" id="cover">
     <Heading size={1} fit caps lineHeight={1} textColor="background">
-      Encapsulation and theming
+      {isFEVR ? 'Component-driven' : 'Encapsulation and theming'}
     </Heading>
     <Text margin="1em 0 0" textColor="background" size={1} fit>
-      the new frontiers of CSS' latest challenge
+      {isFEVR ? 'Web Theming' : 'the new frontiers of CSS\' latest challenge'}
     </Text>
     <Text margin="3em 0 0" textColor="background">Massimo Artizzu</Text>
     <CoverLogo/>
